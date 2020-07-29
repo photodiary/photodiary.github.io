90d7888c7a38db8491159fc9bd9487945b563fb0
Agnieszka Staroń <astaron98@gmail.com>
2020-07-29 22:49:09 +0200

    Updates

---
+++ precache-manifest.bae37dbbd59dfdaa1b8b01dc4433bf44.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bf1693539e589069efc08102f42624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b05e9da92ddfcb42956",
+    "url": "/static/css/2.1a8daee9.chunk.css"
+  },
+  {
+    "revision": "5cb9d0b5567c97ab3194",
+    "url": "/static/css/main.267e7689.chunk.css"
+  },
+  {
+    "revision": "2b05e9da92ddfcb42956",
+    "url": "/static/js/2.677f409f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.677f409f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb9d0b5567c97ab3194",
+    "url": "/static/js/main.34faf256.chunk.js"
+  },
+  {
+    "revision": "b94a12c7f557ee0a87ae",
+    "url": "/static/js/runtime-main.5320c4da.js"
+  },
+  {
+    "revision": "74f02ed3fa6a37972a6254d5a3353317",
+    "url": "/static/media/bg1.74f02ed3.jpg"
+  },
+  {
+    "revision": "4fbeb64b74d90f79efcd3d50ebde02a8",
+    "url": "/static/media/bg2.4fbeb64b.jpg"
+  },
+  {
+    "revision": "1a0203ce45a45b2987767193e00d06be",
+    "url": "/static/media/bg3.1a0203ce.jpg"
+  },
+  {
+    "revision": "7e021e158dc3753022cb25081d1771cd",
+    "url": "/static/media/bg4.7e021e15.jpg"
+  },
+  {
+    "revision": "2ee0cd398cd498d8c1667765e5a43498",
+    "url": "/static/media/bg5.2ee0cd39.jpg"
+  },
+  {
+    "revision": "01f84742f51e5fea9408dfb71ce066a4",
+    "url": "/static/media/bg6.01f84742.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da01b324cbe26cb591b5f74abcd5e523.js"
+  "/precache-manifest.bae37dbbd59dfdaa1b8b01dc4433bf44.js"
 );
 
 self.addEventListener('message', (event) => {
