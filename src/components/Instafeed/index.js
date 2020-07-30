9b5a195749a09755280808fb5224dc6aeb57dea0
Agnieszka Staroń <astaron98@gmail.com>
2020-07-30 20:04:45 +0200

    Add instagram widgets

--- src/components/Instafeed/index.js
+++ src/components/Instafeed/index.js
@@ -3,15 +3,22 @@ import './style.css';
 
 const Instafeed = (props) => {
     return (
-        <div style={{marginTop: "35px"}}>
+        <div className="instagramContent">
             <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
             <div style={{ marginBottom: "10px" }}>
                 <a href="https://www.instagram.com/annanogalska.photodiary/" className="instagram-link fab fa-instagram"><a className="instagram-name">Instagram</a></a>
             </div>
-            <iframe src="//lightwidget.com/widgets/0d6d51756b575edf8e12e076aceaca35.html"
+            <iframe src="//lightwidget.com/widgets/a38a9f9196b1558fa1b20920e57253b0.html"
                 scrolling="no" allowtransparency="true" className="lightwidget-widget"
                 style={{ width: "100%", border: "0", overflow: "hidden" }}>
             </iframe>
+            <br/>
+            <br/>
+            <iframe src="//lightwidget.com/widgets/c526c8f27f7b53fbb546e8c7ea211640.html"
+                scrolling="no" allowtransparency="true" className="lightwidget-widget"
+                style={{ width: "100%", border: "0", overflow: "hidden" }}>
+            </iframe>
+            <br/><br/>
         </div>
     );
 
