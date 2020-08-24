253d71d5f000bac4586014e7783b7b1dd0ea4fe5
Agnieszka Staroń <astaron98@gmail.com>
2020-08-24 19:13:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f3f07560d4759232937ac813e4efe7a.js"
+  "/precache-manifest.c29394a2894fbefb794a703147c93896.js"
 );
 
 self.addEventListener('message', (event) => {
